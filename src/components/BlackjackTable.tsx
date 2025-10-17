9042afe6b6982f627af3f46b165029eaaa95395b
uzsakj <feszoj18@gmail.com>
2025-10-17 22:31:40 +0200

    modified layout of table

--- src/components/BlackjackTable.tsx
+++ src/components/BlackjackTable.tsx
@@ -7,16 +7,18 @@ import { Deck } from "./Deck";
 
 export const BlackjackTable: FC = (): React.ReactNode => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-green-700 p-6">
+        <div className="min-h-screen flex flex-col items-center justify-between bg-green-700 p-6">
             <h1 className="text-4xl font-bold mb-6 text-white">♠ Blackjack ♣</h1>
-            <div className="flex items-center space-x-3">
+            <div className="flex items-center justify-between space-x-3">
                 <div className="flex  flex-col">
                     <DealerHand />
                     <PlayerHand />
                 </div>
                 <Deck />
             </div>
-            <Controls />
+            <div className="mt-6">
+                <Controls />
+            </div>
         </div>
     );
 };
