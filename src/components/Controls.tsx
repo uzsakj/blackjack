c1fdcfc8b7ea3ee79c3b24fcc4d2a6e66f269109
uzsakj <feszoj18@gmail.com>
2025-10-17 21:01:34 +0200

    updated controls

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React from "react";
+import React, { FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { startGame, playerHit, playerStand, dealerTurn } from "@/features/blackjack/blackjackSlice";
 
-export const Controls = () => {
+export const Controls: FC = (): React.ReactNode => {
     const dispatch = useDispatch();
     const { phase, result } = useSelector((s: RootState) => s.blackjack);
 
